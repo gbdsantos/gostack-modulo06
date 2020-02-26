bac7f9c5b1ff5ea3de433df824bb3d3dc37a67bf
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-02-25 22:24:08 -0300

    [gostackmodulo06] Fetching API Data

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
-
+import api from '../../services/api';
 // import { Container } from './styles';
 
 export default class User extends Component {
@@ -8,12 +9,28 @@ export default class User extends Component {
     title: navigation.getParam('user').name,
   })
 
+  static PropTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  }
+
+  state = {
+    stars: [],
+  }
 
   async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
 
+    const response = api.get(`users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
   }
 
   render() {
+
+    const { stars } = this.state;
     return <View />;
   }
 }
